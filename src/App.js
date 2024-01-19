7e1d9beed4ad1ec3fa5c410149b9d658543103c5
jiisuniui <dudndi9752@gmail.com>
2024-01-19 13:11:35 +0900

    ✨ feat(Home): Home 작성

--- src/App.js
+++ src/App.js
@@ -1,18 +1,14 @@
-import { CookiesProvider } from 'react-cookie';
-import React from 'react'; // useState와 useEffect가 사용되지 않으므로 import 제거
-import './App.css';
-import theme from './styles/themes/default';
-import { ThemeProvider } from 'styled-components';
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
-import MainLayout from './layouts/MainLayout';
-import MainPage from './pages/home/MainPage'; // 경로 수정
-import Main from './pages/home/main'; 
-import Homepage from './pages/home/homepage';
-import { PATH_ANY } from './constants';
+import { CookiesProvider } from "react-cookie";
+import React from "react"; // useState와 useEffect가 사용되지 않으므로 import 제거
+import "./App.css";
+import theme from "./styles/themes/default";
+import { ThemeProvider } from "styled-components";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import MainLayout from "./layouts/MainLayout";
+import Home from "./pages/home";
+import { PATH_ANY } from "./constants";
 
 function App() {
-  // 상태 관리 및 API 연결 관련 코드는 주석 처리되어 있습니다.
-
   return (
     <CookiesProvider>
       <ThemeProvider theme={theme}>
@@ -20,7 +16,7 @@ function App() {
           <div className="App">
             <Routes>
               <Route element={<MainLayout />}>
-                <Route path={PATH_ANY} element={<Main />} /> // 'exact' 제거 및 'path'로 변경
+                <Route path={PATH_ANY} element={<Home />} />
               </Route>
               {/* Admin 관련 라우트도 이곳에 추가 */}
             </Routes>
@@ -31,4 +27,4 @@ function App() {
   );
 }
 
-export default App; // App 컴포넌트를 default로 export
+export default App;
--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -1,14 +1,109 @@
-import React, { useContext, useEffect, useState } from 'react';
-import { isMobile } from 'react-device-detect';
+import React from "react";
+import "./style.css";
 
-function Home() {
+import {
+  logo_devquiz,
+  img_computer_science,
+  img_database,
+  img_design_pattern,
+  img_java,
+  img_jpa,
+  img_spring,
+} from "../../assets/images";
+
+export default function Home() {
   return (
-    <>
-      <div>
-        안녕하세요 devQuiz 입니다.
+    <div className="page">
+      <div className="div">
+        <div className="container">
+          <div className="text-wrapper">Categories</div>
+          <div className="list">
+            <div className="row">
+              <button className="article">
+                <div className="image-container">
+                  <div className="image">
+                    <img
+                      className="img"
+                      alt="database"
+                      src={img_computer_science}
+                    />
+                  </div>
+                </div>
+                <div className="frame">
+                  <div className="title-2">Computer Science</div>
+                  <div className="subtitle">
+                    Test your Computer Science knowledge
+                  </div>
+                </div>
+              </button>
+              <button className="article">
+                <div className="image-container">
+                  <div className="image">
+                    <img className="img" alt="java" src={img_design_pattern} />
+                  </div>
+                </div>
+                <div className="frame">
+                  <div className="title-2">Design Pattern</div>
+                  <div className="subtitle">
+                    Test your Design Pattern knowledge
+                  </div>
+                </div>
+              </button>
+            </div>
+            <div className="row">
+              <button className="article">
+                <div className="image-container">
+                  <div className="image">
+                    <img className="img" alt="database" src={img_database} />
+                  </div>
+                </div>
+                <div className="frame">
+                  <div className="title-2">Database</div>
+                  <div className="subtitle">Test your Database knowledge</div>
+                </div>
+              </button>
+              <button className="article">
+                <div className="image-container">
+                  <div className="image">
+                    <img className="img" alt="java" src={img_java} />
+                  </div>
+                </div>
+                <div className="frame">
+                  <div className="title-2">Java</div>
+                  <div className="subtitle">Test your Java knowledge</div>
+                </div>
+              </button>
+            </div>
+            <div className="row">
+              <button className="article">
+                <div className="image-container">
+                  <div className="image" />
+                  <img className="img" alt="spring" src={img_spring} />
+                </div>
+                <div className="frame">
+                  <div className="title-2">Spring</div>
+                  <p className="subtitle">
+                    <span className="span">Test your Spring knowledge</span>
+                  </p>
+                </div>
+              </button>
+              <button className="article">
+                <div className="image-container-2">
+                  <img className="img" alt="jpa" src={img_jpa} />
+                  <div className="image" />
+                </div>
+                <div className="frame">
+                  <div className="title-2">JPA</div>
+                  <p className="subtitle">
+                    <span className="span">Test your JPA knowledge</span>
+                  </p>
+                </div>
+              </button>
+            </div>
+          </div>
+        </div>
+        <img className="vector-2" alt="Vector" src="vector-200-2.svg" />
       </div>
-    </>
+    </div>
   );
 }
-
-export default Home;
