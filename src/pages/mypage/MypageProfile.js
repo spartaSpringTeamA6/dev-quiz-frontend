dcec8ec3aa32f5bd31a3444f0866e64082897644
jiisuniui <dudndi9752@gmail.com>
2024-01-30 22:00:12 +0900

    :recycle: refactor: mypage grass refactor

--- src/pages/mypage/MypageProfile.js
+++ src/pages/mypage/MypageProfile.js
@@ -568,7 +568,8 @@ export default function MyPageProfile() {
                 from="2024-01-01"
                 to="2024-12-31"
                 emptyColor="#eeeeee"
-                colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+                // colors={["#61cdbb", "#97e3d5", "#e8c1a0", "#f47560"]}
+                colors={["#9BE8A8", "#40C463", "#30A14E", "#216E39"]}
                 margin={{ top: 40, right: 40, bottom: 40, left: 40 }}
                 yearSpacing={40}
                 monthBorderColor="#ffffff"
